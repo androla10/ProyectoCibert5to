20549bb6eef915c89864b0bfadd0d69303554c62
Benji Villarreal <benji_zeler@hotmail.com>
2016-04-20 15:30:44 -0500

    Replicativo

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/GDI/js/general.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/GDI/js/general.js
@@ -1,14 +1,17 @@
-$(document).ready(function() {
-//	$(".list-group").children().css("color", "blue");
-//	$(".list-group").children("a").each(function(index,element){
-//		alert($(this).val() + " : "+ $(this).text() +" : ");
-//		var html = $(element).html().substring(0,$(element).html().indexOf("&nbsp;",0));
-//		html.html().removeClass("fa-fw").addClass("fa-2x");
-//		alert(html);
-//		$(element).html(html+"&nbsp"+ "");
-//		$(this).html("<i>HOLA!</i>");
-//	});
+$(function() {
+
+	 var dispositivo = navigator.userAgent.toLowerCase();
+      if( dispositivo.search(/iphone|ipod|ipad|android/) < -1 ){
+			$('.aside').hover(function() {
+					$('.palabra').css('display', 'inline-block');
+					$(this).css('width', '180px');
+					$('body').css('padding-left', '180px');
+				}, function() {
+					// vuelve a dejar el <div> como estaba al hacer el "mouseout"
+					$('.palabra').css('display', 'none');
+					$(this).css('width', '50px');
+					$('body').css('padding-left', '50px');
+				});
+       }
 	
-//	$(".list-group").parent().removeClass("col-md-2").addClass("col-md-1");
-//	alert();
 });
